a1965a3c2346f4149e83966237e05fbf29bcdd82
Gargkrishna24 <gargkrishna888@gmail.com>
2024-03-20 00:27:16 +0530

    Performing some Basic Question using Methods

--- src/function/HCFLCMCalculator.java
+++ src/function/HCFLCMCalculator.java
@@ -4,7 +4,9 @@ public class HCFLCMCalculator {
     public static void main(String[] args) {
         HCFLCMCalculator hcflcmCalculator = new HCFLCMCalculator() ;
         int lcm = hcflcmCalculator.lcm(78, 65);
+        System.out.println("lcm = " + lcm);
         int hcf = hcflcmCalculator.hcf(100, 21);
+        System.out.println("hcf = " + hcf);
     }
 
     public int hcf(int num1, int num2) {
